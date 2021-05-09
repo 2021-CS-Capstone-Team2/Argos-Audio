e3a4771bcece3572ca2b71c7013c79128be67900
soe-un <48954551+soe-un@users.noreply.github.com>
2021-05-09 17:17:22 +0900

    Update speech_recog_tmp.py

--- speech_recog_tmp.py
+++ speech_recog_tmp.py
@@ -4,14 +4,29 @@
 import speech_recognition as sr
 import time
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("MIC ON")
-    audio = r.listen(source)
+def main():
+    r = sr.Recognizer()
+    typelist = ("file","mic")
+    c = "mic"
+    filename = "korean"
+    
+    if( (c in typelist) and c == "file") :
+        with sr.AudioFile(filename+".wav") as source:
+            audio = r.record(source)
+    elif ( (c in typelist) and c == "mic") :
+        with sr.Microphone() as source:
+            print("WAIT ...")
+            r.adjust_for_ambient_noise(source)
+            print("ambient noise check OK, PLEASE TELL ME SOMETHING")
+            audio = r.listen(source)
 
-try:
-    print("You said: "+r.recognize_google(audio, language="ko-KR"))
-except sr.UnknownValueError:
-    print("음성 정보를 인식하지 못했습니다.")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}". format(e))
+    try: #원하면 say return 하면 됨
+        say = r.recognize_google(audio, language="ko-KR")
+        print("You said: "+say)
+    except sr.UnknownValueError:
+        print("음성 정보를 인식하지 못했습니다.")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}". format(e))
+
+if __name__ == "__main__":
+    main()
